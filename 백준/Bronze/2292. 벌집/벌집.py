dba45b2d3015e08bb4c6295c4a3fca68c273b0e3
jhee00 <130778865+jehee0223@users.noreply.github.com>
2024-02-04 16:21:21 +0900

    [Bronze II] Title: 벌집, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.py
+++ 백준/Bronze/2292. 벌집/벌집.py
@@ -1,11 +1,7 @@
 num=int(input())
-a=[1,2]
-add=1
-while num!=1:
-    a.append(a[add]+6*add)
-    if num < a[add+1]:
-        print(add+1)
-        break
-    add+=1
-if num==1:
-    print(1)
+i=1
+count=1
+while(i<num):
+    i+=count*6
+    count+=1
+print(count)
