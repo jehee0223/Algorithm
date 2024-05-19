45469db98c6f9fef48dcd05dfa535d39c05afa33
jhee00 <130778865+jehee0223@users.noreply.github.com>
2024-05-20 02:34:52 +0900

    [Silver IV] Title: ATM, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -3,10 +3,7 @@ cnt = int(input())
 times = list(map(int, input().split()))
 
 times.sort()
-total = list()
+total=0
 for i in range(cnt):
-    if(i==0):
-        total.append(times[i])
-    else:
-        total.append(total[i-1]+times[i])
-print(sum(total))
+    total+=sum(times[0:i+1])
+print(total)
